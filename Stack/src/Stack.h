1f7bfaa8e87a779cfcfd05506d7000a38e931fdd
erdemtuna <erdemtuna@yandex.com>
2019-08-19 16:43:29 +0300

    refinements in stack

--- Stack/src/Stack.h
+++ Stack/src/Stack.h
@@ -12,74 +12,94 @@ class Stack {
     T top(void) const;
     bool empty(void) const;
     bool full(void) const;
-    void print(void) const;
+    void print(std::ostream& os) const;
   private:
-    static const int MAX = 50;
-    T list[MAX];
-    int topPtr;
+    static const std::size_t MAX_SIZE = 50;
+    T list[MAX_SIZE];
+    T* topPtr;
+    std::size_t size;
 };
 
 template <class T>
 Stack<T>::Stack(void) {
-    this->topPtr = -1;
+    topPtr = nullptr;
+    size = 0;
 }
 
 template <class T>
 Stack<T>::Stack(const Stack<T>& copyStack) {
-    this->topPtr = -1;
-    if(!copyStack.empty()){
-        for(int i=0; i<=49; i++) {
-            this->push(copyStack.list[i]);
+    topPtr = nullptr;
+    size = 0;
+    if(!copyStack.empty()) {
+        for(std::size_t i=0; i<copyStack.size; i++) {
+            push(copyStack.list[i]);
         }
     }
+
 }
 
 template <class T>
 void Stack<T>::push(const T& item) {
-
-    if(this->topPtr == this->MAX - 1) {
+    if(size == MAX_SIZE) {
         std::cerr << "Stack overflow. Can't push" << '\n';
     } else {
-        this->topPtr += 1;
-        this->list[topPtr] = item;
+        if(topPtr == nullptr) {
+            topPtr = list;
+        } else {
+            topPtr += 1;
+        }
+        size += 1;
+        *topPtr = item;
     }
 }
 
 template <class T>
 void Stack<T>::pop(void) {
-    if(this->topPtr < 0) {
+    if(topPtr == nullptr) {
         std::cerr << "Stack is empty. Can't pop" << '\n';
     } else {
-        this->topPtr -= 1;
+        if(topPtr == list) {
+            topPtr = nullptr;
+        } else {
+            topPtr -= 1;
+        }
+        size -= 1;
     }
 }
 
 template <class T>
 void Stack<T>::clear(void) {
-    this->topPtr = -1;
+    topPtr = nullptr;
+    size = 0;
 }
 
 template <class T>
 T Stack<T>::top(void)const {
-    if(this->topPtr < 0) {
+    if(topPtr == nullptr) {
         std::cerr << "Stack is empty. No top element." << '\n';
     } else {
-        return this->list[topPtr];
+        return *topPtr;
     }
 }
 
 template <class T>
 bool Stack<T>::empty(void) const {
-    return (this->topPtr == -1);
+    return (topPtr == nullptr);
 }
 
 template <class T>
 bool Stack<T>::full(void) const {
-    return (this->topPtr == this->MAX -1);
+    return (size == MAX_SIZE);
 }
 
 template <class T>
-void Stack<T>::print(void) const {
-    for (int i = this->topPtr; i >= 0; i--)
-        std::cout << this->list[i] << '\n';
+void Stack<T>::print(std::ostream& os) const {
+    os << "size: " << size << '\n';
+    if(size) {
+        for (std::size_t i = 0; i < size; i++)
+            os << *(topPtr - i) << '\n';
+    } else {
+        os << "Stack is empty, nothing to print." << '\n';
+    }
+
 }
